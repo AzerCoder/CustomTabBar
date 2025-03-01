99002620516e2b5fc9874a7f3868cf407ce146c3
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2025-03-01 22:57:52 +0500

    Initial Commit

---
+++ CustomTabBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomTabBar
+//
+//  Created by A'zamjon Abdumuxtorov on 01/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by A'zamjon Abdumuxtorov on 01/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
